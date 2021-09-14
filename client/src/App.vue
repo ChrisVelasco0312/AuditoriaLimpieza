ab7f0f68b5cdf3fe11f2b0e925adeff8d6885510
cristianV12 <cristianvelasco0312@gmail.com>
2021-09-13 23:14:57 -0500

    Front end configuration

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- PrimeVue Components will go here -->
+    <div class="welcome">
+      <h1 class="title">Auditoria Limpieza</h1>
+      <Button label="HOLA MUNDO" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from 'primevue/button'
+
+export default {
+  name: 'app',
+  components: {
+    Button,
+  },
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+#app {
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+
+.welcome {
+  display: grid;
+  justify-content: center;
+}
+</style>
