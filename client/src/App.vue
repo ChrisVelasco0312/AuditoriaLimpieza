e2746801513b24d7a4792b61a75991da24fdd013
cristianV12 <cristianvelasco0312@gmail.com>
2021-09-14 00:58:42 -0500

    Some updates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
     <!-- PrimeVue Components will go here -->
-    <div class="welcome">
+    <div class="welcome p-5 animation-duration-1000 fadein">
       <h1 class="title">Auditoria Limpieza</h1>
-      <Button label="HOLA MUNDO" />
+
+      <h4>Escriba algo a continuación:</h4>
+      <div class="card grid flex justify-content-center align-items-center p-1">
+        <InputText type="text" v-model="text" class="col m-1" />
+      </div>
+      <Card>
+        <template #title> Advanced Card </template>
+        <template #subtitle> {{ text }} </template>
+        <template #content>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          doloribus dolor consequatur ducimus nulla sequi perspiciatis quasi
+          quia enim voluptatum!
+        </template>
+        <template #footer>
+          <Button icon="pi pi-check" label="Save" />
+          <Button
+            icon="pi pi-times"
+            label="Cancel"
+            class="p-button-secondary"
+            style="margin-left: 0.5em"
+          />
+        </template>
+      </Card>
     </div>
   </div>
 </template>
 
 <script>
 import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Card from 'primevue/card'
+import 'primeflex/primeflex.css'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     Button,
+    InputText,
+    Card,
+  },
+  data: () => {
+    return {
+      text: '',
+    }
   },
 }
 </script>
@@ -22,17 +54,20 @@ export default {
 <style>
 body {
   margin: 0;
+  font-family: 'Helvetica';
 }
 
 #app {
   height: 100vh;
   display: grid;
-  justify-content: center;
-  align-items: center;
+  place-items: center;
 }
 
 .welcome {
   display: grid;
   justify-content: center;
+  grid-gap: 1rem;
+  width: 80%;
+  min-width: 300px;
 }
 </style>
