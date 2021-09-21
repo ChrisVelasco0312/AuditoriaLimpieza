c91281166141798145eb70137ac8b9ed9041d247
Jorge Luis Argel Raciny <jargelr@gmail.com>
2021-09-21 15:56:57 -0500

    Se agregó la carpeta models, y su correspondiente archivo auditoria.js)

---
+++ server/models/auditoria.js
@@ -0,0 +1,4 @@
+module.exports = {
+    // db: "mongodb://localhost:27017/vuecrudmevn",
+    //db:"mongodb+srv://faac:SuperMongolico2021@cluster0.1rcdw.mongodb.net/laprimeraMongoDB?retryWrites=true&w=majority",
+};
