0980adbc151755e192dc2e9b03de455b99500bbc
jorgeargel <51498835+jorgeargel@users.noreply.github.com>
2021-09-23 15:14:06 -0500

    Jorge79 (#16)
    * insercion del logo en autenticacion
    
    * Se agregó la carpeta models, y su correspondiente archivo auditoria.js)
    
    * Estructura del Modelo Auditoria

---
+++ server/models/auditoria.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let auditorSchema = new Schema(
+  {
+    nombreAuditor: {
+      type: String,
+    },
+    sede: {
+      type: String,
+    },
+    aseador: {
+      type: String,
+    },
+
+    fecha: {
+      type: Date,
+    },
+    silla: {
+      type: Boolean,
+    },
+    camilla: {
+      type: Boolean,
+    },
+    escritorio: {
+      type: Boolean,
+    },
+    lavamanos: {
+      type: Boolean,
+    },
+    soporte: {
+      type: Boolean,
+    },
+    porcentajeSucio: {
+      type: Number,
+    },
+    porcentajeLimpio: {
+      type: Number,
+    },
+  },
+  {
+    collection: 'auditorias',
+  }
+)
+
+module.exports = mongoose.model('Auditoria', auditorSchema)
+
